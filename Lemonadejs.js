e1f49644f457b8a0af65af52a53799538a266831
Adam <adamhool414@gmail.com>
2016-01-14 14:55:43 -0600

    working on cousole log not html

--- Lemonadejs.js
+++ Lemonadejs.js
@@ -1,29 +1,26 @@
 "use strict";
 
 var moneyIHave=20;
+var cupPrice=0;
+var daysLeft=3;
+
 document.getElementById('money').innerHTML=moneyIHave;
+randomeWeatherForcast();
 
-function goods(price,daysTillExperation,quantity)
+function setLemonadePrice()
 {
-	var inStock;
-	inStock=false;
+    var lemonadePrice;
+    lemonadePrice=prompt("how much do you want to sell a cup of lemonade for?");
+    document.getElementById('todayCupPrice').innerHTML=lemonadePrice;
+    cupPrice=parseFloat(lemonadePrice);
+    console.log(cupPrice);
+    return cupPrice;
+}
+
+function goods(price,quantity)
+{
     this.price=price;
-    this.daysTillExperation=daysTillExperation;
     this.quantity=quantity;
-    this.isExpired=false;
-
-    function goneBad()
-    {
-    	if(this.daysTillExperation=0)
-    	{
-    		this.isExpired=true;
-    	}
-    }
-
-    function getPrice(item)
-    {
-     	return this.price;
-    }
 
     function checkIfOutOfStock(item)
     {
@@ -31,93 +28,179 @@ function goods(price,daysTillExperation,quantity)
         {
         	inStock=true;
         }
-
         else
         {
         	inStock=false;
         }
     }
-
-
 }
 
-var lemons = new goods(1,1,0);
-var ice = new goods(1,1,0);
-var sugar = new goods(2,5,0);
-var cups = new goods(1,100,0);
-
-function getLemonQuantity()
+function randomeWeatherForcast()
 {
-    document.getElementById('lemonsInStock').innerHTML=lemons.quantity;
+    var num=Math.floor((Math.random() * 3) + 1);
+    if(num===1)
+    {
+        document.getElementById('forcast').innerHTML="hot";
+        return "hot";
+    }
+    else if(num===2)
+    {
+        document.getElementById('forcast').innerHTML="cool";
+        return "cool";
+    }
+    else if (num==3)
+    {
+        document.getElementById('forcast').innerHTML="stormy";
+        return "stormy";
+    }
+
 }
 
-function getIceQunatity()
+function determinPotentialCustomers(weather)
 {
-    document.getElementById('iceInStock').innerHTML=ice.quantity;
+    var customers = [];//can not seet property of 0 to undifined
+    if(weather==="hot")
+    {
+        for(var x=0;x<100;x++)
+        {
+            customers[x]=Math.floor((Math.random() * 100) + 1);
+        }
+    }
+    else if(weather==="cool")
+    {
+        for(var x=0;x<50;x++)
+        {
+            customers[x]=Math.floor((Math.random() * 100) + 1);
+        }
+    }
+    else if(weather==="stormy")
+    {
+        for(var x=0;x<25;x++)
+        {
+            customers[x]=Math.floor((Math.random() * 100) + 1);
+        } 
+    }
+    console.log(customers);
+    return customers;
 }
 
-function getSugarQuantity()
+function buyCupsofLemonade(customers)
 {
-    document.getElementById('sugarInStock').innerHTML=sugar.quantity;
-}
+    for(var x=0;x<customers.length;x++)
+    {
+        var chance=(50+(cupPrice*10))
+        if(customers[x]>chance)
+        {
+            console.log(customers[x]);
+            if(lemons.quantity>0&&ice.quantity>0&&sugar.quantity>0&&cups.quantity>0)
+            {
+                console.log(moneyIHave, cupPrice, "above");
+                moneyIHave=moneyIHave+cupPrice;
+                console.log(moneyIHave, cupPrice, "below");
+                lemons.quantity=lemons.quantity-1;
+                console.log(lemons.quantity);
+                document.getElementById('lemonsInStock').innerHtml=lemons.quantity;
+                //console.log('updated lemons in stock=>'+lemons.quantity +'element value =>'+document.getElementById('lemonsInStock').innerHtml)
+                ice.quantity=ice.quantity-1;
+                document.getElementById('iceInStock').innerHtml=ice.quantity;
+                sugar.quantity=sugar.quantity-1;
+                document.getElementById('sugarInStock').innerHtml=sugar.quantity;
+                cups.quantity=cups.quantity-1;
+                document.getElementById('cupsInStock').innerHtml=cups.quantity;
+                document.getElementById('money').innerHtml=moneyIHave;
+                console.log("money i have0", moneyIHave);
 
-function getCupsQuantity()
-{
-    document.getElementById('cupsInStock').innerHTML=cups.quantity;
+            }
+        }
+    }
+    
 }
+var hot;//100 randome dice rolls
+var cool;//50 randome dice rolls
+var stormy;//25 randome dice rolls
+var day;
+
+var lemons = new goods(0.75,0);
+var ice = new goods(0.25,0);
+var sugar = new goods(1.25,0);
+var cups = new goods(0.25,0);
 
 function buyLemons()
 {
-    lemons.quantity++;
+    if(moneyIHave-lemons.price<=0)
+    {
+        console.log("your out of money");
+        return;
+    }
+    lemons.quantity=lemons.quantity+10;
+    moneyIHave-=lemons.price;
+    document.getElementById('money').innerHTML=moneyIHave;
     document.getElementById('lemonsInStock').innerHTML=lemons.quantity;
 }
 
 function buyIce()
 {
-    ice.quantity++;
+    if(moneyIHave-ice.price<=0)
+    {
+        console.log("your out of money");
+        return;
+    }
+    ice.quantity=ice.quantity+10;
+    moneyIHave-=ice.price;
+    document.getElementById('money').innerHTML=moneyIHave;
+
     document.getElementById('iceInStock').innerHTML=ice.quantity;
 }
 
 function buySugar()
 {
-    sugar.quantity++;
+    if(moneyIHave-sugar.price<=0)
+    {
+        console.log("your out of money");
+        return;
+    }
+    sugar.quantity=sugar.quantity+10;
+    moneyIHave-=sugar.price;
+    document.getElementById('money').innerHTML=moneyIHave;
+
     document.getElementById('sugarInStock').innerHTML=sugar.quantity;
 }
 
 function buyCups()
 {
-    cups.quantity++;
+    if(moneyIHave-cups.price<=0)
+    {
+        console.log("your out of money");
+        return;
+    }
+    cups.quantity=cups.quantity+10;
+    moneyIHave-=cups.price;
+    document.getElementById('money').innerHTML=moneyIHave;
+
     document.getElementById('cupsInStock').innerHTML=cups.quantity;
 }
-
-function getWeather()
-{
-var num=Math.floor((Math.random() * 51) + 50);
-	return num;
-}
-
-
-
-function goShopping(itemOut)
-{
-    var stock;
-    stock=itemOut;
-    //money-=;
-}
-
-
-
 function nextDay()
 {
-
+    ice.quantity=0;
+    if(daysLeft>0)
+    {
+        daysLeft--;
+    }
+    else
+    {
+        console.log("Game is over.  You made ", moneyIHave);
+    }
 }
 
-function buyCup()
-{
-
-}
 
 function main()
 {
+    var potentialCustomers;
+    var weather;
 
+    cupPrice=setLemonadePrice();
+    weather=randomeWeatherForcast();
+    potentialCustomers=determinPotentialCustomers(weather);
+    buyCupsofLemonade(potentialCustomers);
+    nextDay(daysLeft);
 }
